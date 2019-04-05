e484f806f993cc0119be0dcac92d45878ea0589c
Henrik Thue Strocka <216976@usn.no>
2019-04-05 18:42:07 +0200

    Added eve auth support

--- BovrilAuthentication/Controllers/HomeController.cs
+++ BovrilAuthentication/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EveOpenApi;
+using EveOpenApi.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,27 @@ namespace BovrilAuthentication.Controllers
 {
 	public class HomeController : Controller
 	{
+		EveWebLogin login;
+
+		public HomeController(ILogin login)
+		{
+			this.login = login as EveWebLogin;
+		}
+
 		public IActionResult Index()
 		{
+			(string authUrl, string state) = login.GetAuthUrl((Scope)"");
+
+			ViewData["AuthURL"] = authUrl;
+			ViewData["Chars"] = login.GetUsers().Count;
 			return View();
+		}
+
+		public async Task<IActionResult> Callback([FromQuery]string code)
+		{
+			await login.AddToken((Scope)"", code);
+			ViewData["Chars"] = login.GetUsers().Count;
+			return View("Index");
 		}
 
 	}
